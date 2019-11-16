8d3a2914fa11731d549c16ae2676ff8b87191702
Paul Haddad <paulh16@gmail.com>
2019-11-16 06:54:53 -0500

    Finish second iteration

--- python_workout/b3-cohort/exercise-1/solution.py
+++ python_workout/b3-cohort/exercise-1/solution.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import pickle
+from xml.etree.ElementTree import ElementTree, Element, SubElement
 import xml.etree.ElementTree as ET
 
 
@@ -31,17 +32,17 @@ class JSONMixin:
 
 
 class CSVMixin:
+    fieldnames = ["title", "author", "price"]
+
     def dump(self, file_path):
         with open(file_path, "w", newline="") as csvfile:
-            fieldnames = ["title", "author", "price"]
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
+            writer = csv.DictWriter(csvfile, fieldnames=CSVMixin.fieldnames)
             writer.writerow(vars(self))
 
 
     def load(self, file_path):
         with open(file_path, "r", newline="") as csvfile:
-            reader = csv.DictReader(csvfile)
+            reader = csv.DictReader(csvfile, fieldnames=CSVMixin.fieldnames)
             attrs = next(reader)
             for attr, value in attrs.items():
                 setattr(self, attr, value)
@@ -49,25 +50,29 @@ class CSVMixin:
 
 class XMLMixin:
     def dump(self, file_path):
-        # with open(file_path, "w") as xmlfile:
-        tree = ElementTree()
-        book_element = ET.Element("book")
-        title_element = ET.SubElement(book_element, "title")
-        author_element = ET.SubElement(book_element, "author")
-        price_element = ET.SubElement(book_element, "price")
+        with open(file_path, "w") as xmlfile:
+            book_element = Element("book")
 
-        title_element.text = self.title
-        author_element.text = self.author
-        price_element.text = str(self.price)
+            title_element = SubElement(book_element, "title")
+            author_element = SubElement(book_element, "author")
+            price_element = SubElement(book_element, "price")
+            title_element.text = self.title
+            author_element.text = self.author
+            price_element.text = str(self.price)
 
-        book_element.write(file_path)
+            book_element.append(title_element)
+            book_element.append(author_element)
+            book_element.append(price_element)
+
+            tree = ElementTree(book_element)
+
+            tree.write(file_path)
 
 
     def load(self, file_path):
-        # with open(file_path, "r") as xmlfile:
-        import pdb; pdb.set_trace()
-        tree = ET.parse(file_path)
-        root = tree.getroot()
-        for child in root:
-            print(child.tag, child.attrib)
+        with open(file_path, "r") as xmlfile:
+            tree = ET.parse(xmlfile)
+            root = tree.getroot()
 
+            for child in iter(root):
+                setattr(self, child.tag, child.text)
--- python_workout/b3-cohort/exercise-1/test_exercise_1.py
+++ python_workout/b3-cohort/exercise-1/test_exercise_1.py
@@ -68,22 +68,22 @@ def test_csv_mixin(tmp_path):
     assert int(b.price) == 100
 
 
-# def test_xml_mixin(tmp_path):
-#     class Book(XMLMixin, Serializable):
-#         def __init__(self, title, author, price):
-#             self.title = title
-#             self.author = author
-#             self.price = price
-#
-#         def __repr__(self):
-#             return f"{self.title}, by {self.author}, for {self.price}"
-#
-#     b = Book('title', 'author', 100)
-#     p = tmp_path / "hello.txt"
-#     b.dump(p)
-#
-#     b.load(p)
-#
-#     assert b.title == 'title'
-#     assert b.author == 'author'
-#     assert int(b.price) == 100
+def test_xml_mixin(tmp_path):
+    class Book(XMLMixin, Serializable):
+        def __init__(self, title, author, price):
+            self.title = title
+            self.author = author
+            self.price = price
+
+        def __repr__(self):
+            return f"{self.title}, by {self.author}, for {self.price}"
+
+    b = Book('title', 'author', 100)
+    p = tmp_path / "hello.txt"
+    b.dump(p)
+
+    b.load(p)
+
+    assert b.title == 'title'
+    assert b.author == 'author'
+    assert int(b.price) == 100
