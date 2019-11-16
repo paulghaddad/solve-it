31576207a60954f6093e4a977c2c08419b21592b
Paul Haddad <paulh16@gmail.com>
2019-11-16 06:26:40 -0500

    Revise exercise 1

--- python_workout/b3-cohort/exercise-1/solution.py
+++ python_workout/b3-cohort/exercise-1/solution.py
@@ -1,32 +1,73 @@
+import csv
 import json
 import pickle
+import xml.etree.ElementTree as ET
 
 
 class Serializable:
     def dump(self, file_path):
         with open(file_path, "wb") as f:
-            pickle.dump(self.__dict__, f)
+            pickle.dump(vars(self), f)
 
 
     def load(self, file_path):
         with open(file_path, "rb") as f:
-            return pickle.load(f)
+            attrs = pickle.load(f)
+            for attr, value in attrs.items():
+                setattr(self, attr, value)
 
 
 class JSONMixin:
     def dump(self, file_path):
-        with open(file_path, "w") as f:
-            json.dump(self.__dict__, f)
+        with open(file_path, "w") as jsonfile:
+            json.dump(vars(self), jsonfile)
 
 
     def load(self, file_path):
-        with open(file_path, "r") as f:
-            return json.load(f)
-
-
-class XMLMixin:
-    pass
+        with open(file_path, "r") as jsonfile:
+            attrs = json.load(jsonfile)
+            for attr, value in attrs.items():
+                setattr(self, attr, value)
 
 
 class CSVMixin:
-    pass
+    def dump(self, file_path):
+        with open(file_path, "w", newline="") as csvfile:
+            fieldnames = ["title", "author", "price"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow(vars(self))
+
+
+    def load(self, file_path):
+        with open(file_path, "r", newline="") as csvfile:
+            reader = csv.DictReader(csvfile)
+            attrs = next(reader)
+            for attr, value in attrs.items():
+                setattr(self, attr, value)
+
+
+class XMLMixin:
+    def dump(self, file_path):
+        # with open(file_path, "w") as xmlfile:
+        tree = ElementTree()
+        book_element = ET.Element("book")
+        title_element = ET.SubElement(book_element, "title")
+        author_element = ET.SubElement(book_element, "author")
+        price_element = ET.SubElement(book_element, "price")
+
+        title_element.text = self.title
+        author_element.text = self.author
+        price_element.text = str(self.price)
+
+        book_element.write(file_path)
+
+
+    def load(self, file_path):
+        # with open(file_path, "r") as xmlfile:
+        import pdb; pdb.set_trace()
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        for child in root:
+            print(child.tag, child.attrib)
+
--- python_workout/b3-cohort/exercise-1/test_exercise_1.py
+++ python_workout/b3-cohort/exercise-1/test_exercise_1.py
@@ -47,27 +47,27 @@ def test_json_mixin(tmp_path):
     assert type(json.load(open(p, 'rb'))) == dict
 
 
-# def test_csv_mixin(tmp_path):
-#     class Book(CSVMixin, Serializable):
-#         def __init__(self, title, author, price):
-#             self.title = title
-#             self.author = author
-#             self.price = price
-#
-#         def __repr__(self):
-#             return f"{self.title}, by {self.author}, for {self.price}"
-#
-#     b = Book('title', 'author', 100)
-#     p = tmp_path / "hello.txt"
-#     b.dump(p)
-#
-#     b.load(p)
-#
-#     assert b.title == 'title'
-#     assert b.author == 'author'
-#     assert int(b.price) == 100
-#
-#
+def test_csv_mixin(tmp_path):
+    class Book(CSVMixin, Serializable):
+        def __init__(self, title, author, price):
+            self.title = title
+            self.author = author
+            self.price = price
+
+        def __repr__(self):
+            return f"{self.title}, by {self.author}, for {self.price}"
+
+    b = Book('title', 'author', 100)
+    p = tmp_path / "hello.txt"
+    b.dump(p)
+
+    book = b.load(p)
+
+    assert b.title == 'title'
+    assert b.author == 'author'
+    assert int(b.price) == 100
+
+
 # def test_xml_mixin(tmp_path):
 #     class Book(XMLMixin, Serializable):
 #         def __init__(self, title, author, price):
