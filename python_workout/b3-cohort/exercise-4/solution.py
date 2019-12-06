350cd2f5f665771c3da6ed7e130d8a4b9a4aabd7
Paul Haddad <paulh16@gmail.com>
2019-12-06 16:11:40 -0500

    Finish out Exercise 4

--- python_workout/b3-cohort/exercise-4/solution.py
+++ python_workout/b3-cohort/exercise-4/solution.py
@@ -1,8 +1,16 @@
-from zipfile import ZipFile
+import tarfile
+from zipfile import ZipFile, ZIP_DEFLATED
 
 
-def tar_to_zip(tarpath, zippath):
+def tar_to_zip(*tarfiles, zippath):
     zipfile = zippath.joinpath("output.zip")
 
-    with ZipFile(zipfile, "w") as zf:
-        zf.write(tarpath)
+    for archive in tarfiles:
+        with ZipFile(zipfile, "w", compression=ZIP_DEFLATED) as zf:
+            try:
+                with tarfile.open(archive, "r") as tf:
+                    for info in tf:
+                        tf.extract(info, path="/tmp/")
+                        zf.write(f"/tmp/{info.name}", arcname=info.name)
+            except tarfile.ReadError as e:
+                print(f"There was an error reading the file {archive}")
