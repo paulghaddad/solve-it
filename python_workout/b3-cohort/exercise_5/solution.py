41eeb6d231170913c36ba7dccd2f8e71e191fd18
Paul Haddad <paulh16@gmail.com>
2019-12-09 11:22:25 -0500

    Exercise 5 * Iteration 2

--- python_workout/b3-cohort/exercise_5/solution.py
+++ python_workout/b3-cohort/exercise_5/solution.py
@@ -1,5 +1,8 @@
 class ImmutableParent():
     def __init__(self, **kwargs):
+        # We could also go above the __setattr__ method of this class and use
+        # object's within this __init__function by using:
+        # object.__setattr__(self, key, value)
         self.__dict__ = kwargs
 
     def __setattr__(self, attr, value):
