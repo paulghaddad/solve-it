5c5f10d5173d51b3066474bc1272d7f5cbcbdbca
Paul Haddad <paulh16@gmail.com>
2020-02-15 11:33:27 -0500

    Add another solution for 217 - Contains Duplicate

--- leetcode/217_contains_duplicate/contains_duplicate.py
+++ leetcode/217_contains_duplicate/contains_duplicate.py
@@ -1,5 +1,20 @@
 # Time Complexity: O(n)
 # Space Complexity O(1)
 
-def contains_duplicate_1(elements):
-    return len(elements) != len(set(elements))
+def contains_duplicate_1(nums):
+    return len(nums) != len(set(nums))
+
+
+# Time Complexity: O(n)
+# Space Complexity O(n)
+
+def contains_duplicate_2(nums):
+    prev_nums = {}
+
+    for num in nums:
+        if num in prev_nums:
+            return True
+
+        prev_nums[num] = True
+
+    return False
--- leetcode/217_contains_duplicate/test_contains_duplicate.py
+++ leetcode/217_contains_duplicate/test_contains_duplicate.py
@@ -1,9 +1,17 @@
 import pytest
 
-from contains_duplicate import contains_duplicate_1
+from contains_duplicate import (
+    contains_duplicate_1,
+    contains_duplicate_2,
+)
 
-def test_contains_duplicate():
+def test_contains_duplicate_1():
     assert contains_duplicate_1([1, 2, 3, 1]) == True
     assert contains_duplicate_1([1, 2, 3, 4]) == False
     assert contains_duplicate_1([1,1,1,3,3,4,3,2,4,2]) == True
 
+
+def test_contains_duplicate_2():
+    assert contains_duplicate_2([1, 2, 3, 1]) == True
+    assert contains_duplicate_2([1, 2, 3, 4]) == False
+    assert contains_duplicate_2([1,1,1,3,3,4,3,2,4,2]) == True
