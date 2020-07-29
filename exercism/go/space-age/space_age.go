2a81b984adf0278814848f00e13874b1362e9116
Paul Haddad <paulh16@gmail.com>
2020-07-29 07:07:43 -0400

    Go - Space Age - 1

---
+++ exercism/go/space-age/space_age.go
@@ -0,0 +1,20 @@
+package space
+
+type Planet string
+
+const SECONDS_TO_EARTH_YEAR = 31557600
+
+var orbital_periods = map[Planet]float64{
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Earth":   1.0,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
+
+func Age(age float64, planet Planet) float64 {
+	return (age / SECONDS_TO_EARTH_YEAR) / orbital_periods[planet]
+}
