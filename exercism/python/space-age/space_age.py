733caf766769df624ad9f331871cf11bcdac5386
Paul Haddad <paulh16@gmail.com>
2020-08-03 06:58:35 -0400

    Python - Space Age - 2
    * Refactor to dynamically create methods

--- exercism/python/space-age/space_age.py
+++ exercism/python/space-age/space_age.py
@@ -1,35 +1,22 @@
 class SpaceAge:
+    earth_years = {
+        'mercury': 0.2408467,
+        'venus': 0.61519726,
+        'earth': 1.0,
+        'mars': 1.8808158,
+        'jupiter': 11.862615,
+        'saturn': 29.447498,
+        'uranus': 84.016846,
+        'neptune': 164.79132,
+    }
+
+    earth_seconds = 31557600
+
+
     def __init__(self, seconds):
         self.age_seconds = seconds
+        self.earth_year = round(self.age_seconds / SpaceAge.earth_seconds, 2)
 
-
-    def on_earth(self):
-        return round(self.age_seconds / 31557600, 2)
-
-
-    def on_mercury(self):
-        return round(self.on_earth() / 0.2408467, 2)
-
-
-    def on_venus(self):
-        return round(self.on_earth() / 0.61519726, 2)
-
-
-    def on_mars(self):
-        return round(self.on_earth() / 1.8808158, 2)
-
-
-    def on_jupiter(self):
-        return round(self.on_earth() / 11.862615, 2)
-
-
-    def on_saturn(self):
-        return round(self.on_earth() / 29.447498, 2)
-
-
-    def on_uranus(self):
-        return round(self.on_earth() / 84.016846, 2)
-
-
-    def on_neptune(self):
-        return round(self.on_earth() / 164.79132, 2)
+        for planet, p_years in SpaceAge.earth_years.items():
+            earth_years = lambda p_years=p_years: round(self.earth_year / p_years, 2)
+            setattr(self, f'on_{planet}', earth_years)
