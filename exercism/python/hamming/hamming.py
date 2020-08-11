2bb8ae696736d57ac53dbfb41affe5068e29d847
Paul Haddad <paulh16@gmail.com>
2020-08-11 06:53:38 -0400

    Python - Hamming - 2
    * Use a list comprehension and sum instead

--- exercism/python/hamming/hamming.py
+++ exercism/python/hamming/hamming.py
@@ -2,10 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('Strands are not the same length.')
 
-    differences = 0
-
-    for i in range(len(strand_a)):
-        if strand_a[i] != strand_b[i]:
-            differences += 1
-
-    return differences
+    return sum(1 for a,b in zip(strand_a, strand_b) if a != b)
