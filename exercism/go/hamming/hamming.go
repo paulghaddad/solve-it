4e0533d61268bf84d4ddf800024da89dea322fc9
Paul Haddad <paulh16@gmail.com>
2020-08-12 06:58:41 -0400

    Go - Hamming - 2
    * Small cleanup

--- exercism/go/hamming/hamming.go
+++ exercism/go/hamming/hamming.go
@@ -3,15 +3,14 @@ package hamming
 import "fmt"
 
 func Distance(a, b string) (int, error) {
-	difference := 0
-
 	if len(a) != len(b) {
-		return difference, fmt.Errorf("The lengths of the two strands are different: %d and %d", len(a), len(b))
+		return -1, fmt.Errorf("The lengths of the two strands are different: %d and %d", len(a), len(b))
 	}
 
+	difference := 0
 	for i := 0; i < len(a); i++ {
 		if a[i] != b[i] {
-			difference += 1
+			difference++
 		}
 	}
 
