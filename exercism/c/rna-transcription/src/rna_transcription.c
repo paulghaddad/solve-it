82995e04297b41ce83452d5ac9a1a90cbc53948f
Paul Haddad <paulh16@gmail.com>
2020-08-15 07:47:56 -0400

    C - RNA Transcription - 1

---
+++ exercism/c/rna-transcription/src/rna_transcription.c
@@ -0,0 +1,35 @@
+#include "rna_transcription.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char *to_rna(const char *dna) {
+  int i, len = strlen(dna);
+  char *rna = (char *)malloc(len * sizeof(char));
+  if (rna == NULL)
+    return NULL;
+
+  for (i = 0; i < len; ++i) {
+    switch (*(dna+i)) {
+      case 'G':
+        *(rna+i) = 'C';
+        break;
+      case 'C':
+        *(rna+i) = 'G';
+        break;
+      case 'T':
+        *(rna+i) = 'A';
+        break;
+      case 'A':
+        *(rna+i) = 'U';
+        break;
+      default:
+        free(rna);
+        return NULL;
+    }
+  }
+
+  *(rna+i) = '\0';
+
+  return rna;
+}
