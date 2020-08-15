6947ada34c5748ad10b320e9b4a3fccddeaaccb5
Paul Haddad <paulh16@gmail.com>
2020-08-15 11:03:38 -0400

    Go - DnaToRna - 2
    * Use bytes package

--- exercism/go/rna-transcription/rna_transcription.go
+++ exercism/go/rna-transcription/rna_transcription.go
@@ -1,20 +1,22 @@
 package strand
 
+import "bytes"
+
 // DnaToRna maps DNA nucleotides to RNA nucleotides
-var DnaToRna = map[string]string{
-	"C": "G",
-	"G": "C",
-	"T": "A",
-	"A": "U",
+var DnaToRna = map[rune]rune{
+	'C': 'G',
+	'G': 'C',
+	'T': 'A',
+	'A': 'U',
 }
 
 // ToRNA transcribes a DNA strand to its RNA complement
 func ToRNA(dna string) string {
-	rna := ""
+	var buf bytes.Buffer
 
-	for i := 0; i < len(dna); i++ {
-		rna += DnaToRna[string(dna[i])]
+	for _, nuc := range dna {
+		buf.WriteRune(DnaToRna[nuc])
 	}
 
-	return rna
+	return buf.String()
 }
