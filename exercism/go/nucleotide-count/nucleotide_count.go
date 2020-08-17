26e0224fc728afbcdda1d5c963e27c71d5d23782
Paul Haddad <paulh16@gmail.com>
2020-08-17 07:00:11 -0400

    Go - Nucleotide Count - 2
    * Simplify logic
    * Remove unnecessary vars
    * Use runes

--- exercism/go/nucleotide-count/nucleotide_count.go
+++ exercism/go/nucleotide-count/nucleotide_count.go
@@ -1,28 +1,26 @@
 package dna
 
-import "errors"
+import "fmt"
 
 // Histogram is a mapping from nucleotide to its count in given DNA.
-type Histogram map[byte]int
+type Histogram map[rune]int
 
 // DNA is a list of nucleotides. Choose a suitable data type.
 type DNA string
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 func (d DNA) Counts() (Histogram, error) {
-	var h = Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
-	var err error = nil
+	h := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
 
 	for _, nuc := range d {
-		_, ok := h[byte(nuc)]
+		_, ok := h[nuc]
 
 		if !ok {
-			err = errors.New("The DNA strand has an invalid nucleotide")
-			return h, err
+			return nil, fmt.Errorf("The DNA strand has an invalid nucleotide: %c", nuc)
 		}
 
-		h[byte(nuc)]++
+		h[nuc]++
 	}
 
-	return h, err
+	return h, nil
 }
