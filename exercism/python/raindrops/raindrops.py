dacd5828f10113ca4852cc5104ca6ea9de52e1cf
Paul Haddad <paulh16@gmail.com>
2020-08-20 06:41:47 -0400

    Python - Raindrops - 3
    * Further refactor by eliminating helper function

--- exercism/python/raindrops/raindrops.py
+++ exercism/python/raindrops/raindrops.py
@@ -1,20 +1,10 @@
-factor_to_sound = {
+sound_factors = {
     3: 'Pling',
     5: 'Plang',
     7: 'Plong',
 }
 
-factors = (3, 5, 7)
-
-
-def add_sound(number, factor):
-    if number % factor == 0:
-        return factor_to_sound[factor]
-    else:
-        return ""
-
-
 def convert(number):
-    sounds = [add_sound(number, factor) for factor in factors]
+    sounds = [sound for factor, sound in sound_factors.items() if number % factor == 0]
 
     return ''.join(sounds) or str(number)
