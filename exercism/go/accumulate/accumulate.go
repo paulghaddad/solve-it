c3410dba854e680b44f37ee9d80a43dfac80349b
Paul Haddad <paulh16@gmail.com>
2020-08-24 08:19:34 -0400

    Wrap up Go - Accumulate and JS - Hello World

--- exercism/go/accumulate/accumulate.go
+++ exercism/go/accumulate/accumulate.go
@@ -4,7 +4,6 @@ package accumulate
 // collection with the operation applied to each element
 func Accumulate(collection []string, converter func(string) string) []string {
 	output := make([]string, len(collection))
-
 	for i, el := range collection {
 		output[i] = converter(el)
 	}
