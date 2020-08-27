e2cfad6210fd62ff9429f645064b64ddd14fc4e3
Paul Haddad <paulh16@gmail.com>
2020-08-27 07:11:45 -0400

    Python - Roman Numeral - 1

---
+++ exercism/python/roman-numerals/roman_numerals.py
@@ -0,0 +1,35 @@
+arabic_to_roman = {
+    1000: 'M',
+    500: 'D',
+    100: 'C',
+    50: 'L',
+    10: 'X',
+    5: 'V',
+    1: 'I',
+}
+
+def roman(number):
+    if number == 0:
+        return ''
+
+    for ar, rom in arabic_to_roman.items():
+        if ar - number > 100 and ar - number < 100:
+            return 'C' + rom + roman(number-(ar-100))
+
+        if 1000 - number > 0 and 1000 - number < 100:
+            return 'CM' + roman(number-900)
+
+        if 500 - number > 0 and 500 - number < 100:
+            return 'CD' + roman(number-400)
+
+        if 100 - number > 0 and 100 - number < 10:
+            return 'XC' + roman(number-90)
+
+        if 50 - number > 0 and 50 - number < 10:
+            return 'XL' + roman(number-40)
+
+        if ar - 1 == number:
+            return 'I' + rom + roman(number-(ar-1))
+
+        if number // ar > 0:
+            return rom + roman(number-ar)
