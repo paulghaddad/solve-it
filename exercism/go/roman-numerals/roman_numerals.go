88b54621b292910dd835ed3102e0f52524b91684
Paul Haddad <paulh16@gmail.com>
2020-09-01 07:14:03 -0400

    Go - Roman Numberal - 2
    * Use binary search

--- exercism/go/roman-numerals/roman_numerals.go
+++ exercism/go/roman-numerals/roman_numerals.go
@@ -1,58 +1,52 @@
 package romannumerals
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+type arabicToRoman struct {
+	arabic int
+	roman  string
+}
+
+var mappings = [13]arabicToRoman{
+	arabicToRoman{1000, "M"},
+	arabicToRoman{900, "CM"},
+	arabicToRoman{500, "D"},
+	arabicToRoman{400, "CD"},
+	arabicToRoman{100, "C"},
+	arabicToRoman{90, "XC"},
+	arabicToRoman{50, "L"},
+	arabicToRoman{40, "XL"},
+	arabicToRoman{10, "X"},
+	arabicToRoman{9, "IX"},
+	arabicToRoman{5, "V"},
+	arabicToRoman{4, "IV"},
+	arabicToRoman{1, "I"},
+}
 
 // ToRomanNumeral converts an arabic number to a roman one
 func ToRomanNumeral(arabic int) (string, error) {
 	if arabic <= 0 || arabic > 3000 {
 		err := fmt.Errorf("The arabic numeral %d cannot be converted to a roman numeral", arabic)
 		return "", err
-
 	}
+	numberMappings := len(mappings)
 
 	roman := ""
 	for arabic > 0 {
-		switch {
-		case arabic >= 1000 && arabic%1000 >= 0:
-			roman += "M"
-			arabic -= 1000
-		case arabic >= 900 && arabic%900 >= 0:
-			roman += "CM"
-			arabic -= 900
-		case arabic >= 500 && arabic%500 >= 0:
-			roman += "D"
-			arabic -= 500
-		case arabic >= 400 && arabic%400 >= 0:
-			roman += "CD"
-			arabic -= 400
-		case arabic >= 100 && arabic%100 >= 0:
-			roman += "C"
-			arabic -= 100
-		case arabic >= 90 && arabic%90 >= 0:
-			roman += "XC"
-			arabic -= 90
-		case arabic >= 50 && arabic%50 >= 0:
-			roman += "L"
-			arabic -= 50
-		case arabic >= 40 && arabic%40 >= 0:
-			roman += "XL"
-			arabic -= 40
-		case arabic >= 10 && arabic%10 >= 0:
-			roman += "X"
-			arabic -= 10
-		case arabic >= 9 && arabic%9 >= 0:
-			roman += "IX"
-			arabic -= 9
-		case arabic >= 5 && arabic%5 >= 0:
-			roman += "V"
-			arabic -= 5
-		case arabic >= 4 && arabic%4 == 0:
-			roman += "IV"
-			arabic -= 4
-		default:
-			roman += "I"
-			arabic--
+		idx := sort.Search(numberMappings, func(i int) bool {
+			return arabic >= mappings[i].arabic
+		})
+
+		if idx == numberMappings {
+			return "", fmt.Errorf("There was an error processing the arabic digit")
+
 		}
+
+		roman += mappings[idx].roman
+		arabic -= mappings[idx].arabic
 	}
 
 	return roman, nil
