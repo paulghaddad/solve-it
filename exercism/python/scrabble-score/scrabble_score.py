afef37a871cce4436fc3c6f75a6be8fc1e4fa4dd
Paul Haddad <paulh16@gmail.com>
2020-09-21 08:39:13 -0400

    Python - Scrabble Score - 1

---
+++ exercism/python/scrabble-score/scrabble_score.py
@@ -0,0 +1,26 @@
+LETTER_VALUES = {
+    'D': 2,
+    'G': 2,
+    'B': 3,
+    'C': 3,
+    'M': 3,
+    'P': 3,
+    'F': 4,
+    'H': 4,
+    'V': 4,
+    'W': 4,
+    'Y': 4,
+    'K': 5,
+    'J': 8,
+    'X': 8,
+    'Q': 10,
+    'Z': 10,
+}
+
+
+def score(word):
+    total = 0
+    for letter in word:
+        total += LETTER_VALUES.get(letter.upper(), 1)
+
+    return total
