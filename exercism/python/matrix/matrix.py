da81093d72f968c954daaf3123a37b8df051b1af
Paul Haddad <paulh16@gmail.com>
2020-09-23 13:54:52 -0400

    Python - Matrix - 2 * Clean up string methods and list comprehensions

--- exercism/python/matrix/matrix.py
+++ exercism/python/matrix/matrix.py
@@ -2,11 +2,14 @@ class Matrix:
     def __init__(self, matrix_string):
         self.matrix = Matrix.process_matrix_string(matrix_string)
 
+
     def row(self, index):
         return self.matrix[index-1]
 
+
     def column(self, index):
-        return [row[index-1] for row in self.matrix]
+        return list([col for col in zip(*self.matrix)][index-1])
+
 
     def process_matrix_string(string):
-        return [[int(el) for el in row.split(' ')] for row in string.split('\n')]
+        return [[int(el) for el in row.split()] for row in string.splitlines()]
