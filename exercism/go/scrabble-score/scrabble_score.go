a56c1adc6c6089161eb75be1398beb6fc0e7e62b
Paul Haddad <paulh16@gmail.com>
2020-09-23 07:21:26 -0400

    Go - Scrabble Score - 2
    * Use array indexing instead of a map

--- exercism/go/scrabble-score/scrabble_score.go
+++ exercism/go/scrabble-score/scrabble_score.go
@@ -2,40 +2,13 @@ package scrabble
 
 import "unicode"
 
-var letterValues = map[rune]int{
-	'a': 1,
-	'e': 1,
-	'i': 1,
-	'o': 1,
-	'u': 1,
-	'l': 1,
-	'n': 1,
-	'r': 1,
-	's': 1,
-	't': 1,
-	'd': 2,
-	'g': 2,
-	'b': 3,
-	'c': 3,
-	'm': 3,
-	'p': 3,
-	'f': 4,
-	'h': 4,
-	'v': 4,
-	'w': 4,
-	'y': 4,
-	'k': 5,
-	'j': 8,
-	'x': 8,
-	'q': 10,
-	'z': 10,
-}
+var letterValues = [26]int{1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10}
 
 // Score calculates the Scrabble score for a word
 func Score(input string) int {
 	total := 0
 	for _, letter := range input {
-		total += letterValues[unicode.ToLower(letter)]
+		total += letterValues[unicode.ToLower(letter)-'a']
 	}
 
 	return total
