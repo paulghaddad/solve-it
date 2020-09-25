7c986063897020d0dbd737e951356a3ec26bb68a
Paul Haddad <paulh16@gmail.com>
2020-09-25 06:55:12 -0400

    Python - Isogram
    * Add alternative bit manipulation solution

--- exercism/python/isogram/isogram.py
+++ exercism/python/isogram/isogram.py
@@ -9,3 +9,22 @@ def is_isogram(string):
             letters.add(letter)
 
     return True
+
+
+# def is_isogram(string):
+#     bs = 0
+#     for c in string:
+#         n = ord(c.lower()) - ord('a')
+#         if not 0 <= n < 26:
+#             continue
+#
+#         print('1 << n', n, 1 << n, bin(1 << n))
+#         if bs & (1 << n):
+#             return False
+#
+#         print('bs & (1 << n)', n, bin(bs & (1 << n)))
+#
+#         bs |= (1 << n)
+#         print('bs |= (1 << n)', bin(bs))
+#
+#     return True
