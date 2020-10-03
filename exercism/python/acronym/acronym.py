ba71394a3a05fcb7db0af657c9d0ca0cb3ec813b
Paul Haddad <paulh16@gmail.com>
2020-10-03 07:09:24 -0400

    Python - Acronym - 2
    * Call upper() once

--- exercism/python/acronym/acronym.py
+++ exercism/python/acronym/acronym.py
@@ -2,4 +2,4 @@ import re
 
 
 def abbreviate(words):
-    return ''.join([word[0].upper() for word in re.split(r'[-_\s]+', words)])
+    return ''.join([word[0] for word in re.split(r'[-_\s]+', words)]).upper()
