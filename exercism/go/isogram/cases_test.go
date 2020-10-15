ccd114cc69ed0aaea3f67c017c415cbf109c6269
Paul Haddad <paulh16@gmail.com>
2020-10-15 07:15:48 -0400

    Go - Isogram - 1
    * Using a bitmap array to detect duplicates

--- exercism/go/isogram/cases_test.go
+++ exercism/go/isogram/cases_test.go
@@ -24,54 +24,54 @@ var testCases = []struct {
 		input:       "eleven",
 		expected:    false,
 	},
-	// {
-	// 	description: "word with one duplicated character from the end of the alphabet",
-	// 	input:       "zzyzx",
-	// 	expected:    false,
-	// },
-	// {
-	// 	description: "longest reported english isogram",
-	// 	input:       "subdermatoglyphic",
-	// 	expected:    true,
-	// },
-	// {
-	// 	description: "word with duplicated character in mixed case",
-	// 	input:       "Alphabet",
-	// 	expected:    false,
-	// },
-	// {
-	// 	description: "word with duplicated character in mixed case, lowercase first",
-	// 	input:       "alphAbet",
-	// 	expected:    false,
-	// },
-	// {
-	// 	description: "hypothetical isogrammic word with hyphen",
-	// 	input:       "thumbscrew-japingly",
-	// 	expected:    true,
-	// },
-	// {
-	// 	description: "hypothetical word with duplicated character following hyphen",
-	// 	input:       "thumbscrew-jappingly",
-	// 	expected:    false,
-	// },
-	// {
-	// 	description: "isogram with duplicated hyphen",
-	// 	input:       "six-year-old",
-	// 	expected:    true,
-	// },
-	// {
-	// 	description: "made-up name that is an isogram",
-	// 	input:       "Emily Jung Schwartzkopf",
-	// 	expected:    true,
-	// },
-	// {
-	// 	description: "duplicated character in the middle",
-	// 	input:       "accentor",
-	// 	expected:    false,
-	// },
-	// {
-	// 	description: "same first and last characters",
-	// 	input:       "angola",
-	// 	expected:    false,
-	// },
+	{
+		description: "word with one duplicated character from the end of the alphabet",
+		input:       "zzyzx",
+		expected:    false,
+	},
+	{
+		description: "longest reported english isogram",
+		input:       "subdermatoglyphic",
+		expected:    true,
+	},
+	{
+		description: "word with duplicated character in mixed case",
+		input:       "Alphabet",
+		expected:    false,
+	},
+	{
+		description: "word with duplicated character in mixed case, lowercase first",
+		input:       "alphAbet",
+		expected:    false,
+	},
+	{
+		description: "hypothetical isogrammic word with hyphen",
+		input:       "thumbscrew-japingly",
+		expected:    true,
+	},
+	{
+		description: "hypothetical word with duplicated character following hyphen",
+		input:       "thumbscrew-jappingly",
+		expected:    false,
+	},
+	{
+		description: "isogram with duplicated hyphen",
+		input:       "six-year-old",
+		expected:    true,
+	},
+	{
+		description: "made-up name that is an isogram",
+		input:       "Emily Jung Schwartzkopf",
+		expected:    true,
+	},
+	{
+		description: "duplicated character in the middle",
+		input:       "accentor",
+		expected:    false,
+	},
+	{
+		description: "same first and last characters",
+		input:       "angola",
+		expected:    false,
+	},
 }
--- exercism/go/isogram/isogram.go
+++ exercism/go/isogram/isogram.go
@@ -1,26 +1,23 @@
 package isogram
 
 import (
-	"fmt"
 	"strings"
+	"unicode"
 )
 
-// IsIsogram determines whether a string is an isogram
+// IsIsogram returns whether a word or phrase is an isogram
 func IsIsogram(str string) bool {
-	if str == "" {
-		return false
-	}
-
-	lettersPresent := 0 << 26
+	var letterPresent [26]int
 
 	for _, char := range strings.ToLower(str) {
-		bitOffset := char << (char - 'a')
-		fmt.Printf("Char value: %d\n", bitOffset)
-		if lettersPresent&int(bitOffset) > 0 {
-			return false
-		}
+		if unicode.IsLetter(char) {
+			offset := int(char) - 'a'
+			if letterPresent[offset] > 0 {
+				return false
+			}
 
-		lettersPresent |= int(bitOffset)
+			letterPresent[offset] = 1
+		}
 	}
 
 	return true
