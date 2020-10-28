b6d3d645912ee4c1e36a2f78c74f25aa3cd88041
Paul Haddad <paulh16@gmail.com>
2020-10-28 06:59:02 -0400

    Python - Scale Generator - 1

--- exercism/python/scale-generator/scale_generator.py
+++ exercism/python/scale-generator/scale_generator.py
@@ -2,12 +2,14 @@ class Scale:
     CHROMATIC_WITH_SHARPS = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
     CHROMATIC_WITH_FLATS = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
 
+    DIATONIC_SHARPS = ('A', 'a', 'C', 'G', 'f#')
+
 
     def __init__(self, tonic):
         self.tonic = tonic
 
     def chromatic(self):
-        if self.tonic in ('A', 'a', 'C', 'G', 'f#'):
+        if self.tonic in self.DIATONIC_SHARPS:
             tonic = self.tonic[0].upper() + self.tonic[1:]
             index = Scale.CHROMATIC_WITH_SHARPS.index(tonic)
             return Scale.CHROMATIC_WITH_SHARPS[index:] + Scale.CHROMATIC_WITH_SHARPS [:index]
@@ -18,16 +20,16 @@ class Scale:
 
     def interval(self, intervals):
         chromatic = self.chromatic()
-        scale = []
 
         i = 0
+        scale = []
         for interval in intervals:
             scale.append(chromatic[i])
-            if interval == 'M':
+            if interval == 'm':
+                i += 1
+            elif interval == 'M':
                 i += 2
             elif interval == 'A':
                 i += 3
-            else:
-                i += 1
 
         return scale
