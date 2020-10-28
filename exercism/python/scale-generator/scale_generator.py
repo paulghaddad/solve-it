9686a1d446c518f8f27d8ccfad3dc8d9a8ee0091
Paul Haddad <paulh16@gmail.com>
2020-10-28 07:25:23 -0400

    Python - Scale Generator - 2
    * A lot of refactoring

--- exercism/python/scale-generator/scale_generator.py
+++ exercism/python/scale-generator/scale_generator.py
@@ -1,22 +1,19 @@
 class Scale:
-    CHROMATIC_WITH_SHARPS = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-    CHROMATIC_WITH_FLATS = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
+    CHROMATIC_SHARPS = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    CHROMATIC_FLATS = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
 
     DIATONIC_SHARPS = ('A', 'a', 'C', 'G', 'f#')
+
+    INTERVAL_DISTANCES = ('m', 'M', 'A')
 
 
     def __init__(self, tonic):
         self.tonic = tonic
+        self.scale = self._get_scale_by_tonic(self.tonic)
+        self.index = self.scale.index(self.tonic.capitalize())
 
     def chromatic(self):
-        if self.tonic in self.DIATONIC_SHARPS:
-            tonic = self.tonic[0].upper() + self.tonic[1:]
-            index = Scale.CHROMATIC_WITH_SHARPS.index(tonic)
-            return Scale.CHROMATIC_WITH_SHARPS[index:] + Scale.CHROMATIC_WITH_SHARPS [:index]
-        else:
-            tonic = self.tonic[0].upper() + self.tonic[1:]
-            index = Scale.CHROMATIC_WITH_FLATS.index(tonic)
-            return Scale.CHROMATIC_WITH_FLATS[index:] + Scale.CHROMATIC_WITH_FLATS [:index]
+        return self.scale[self.index:] + self.scale[:self.index]
 
     def interval(self, intervals):
         chromatic = self.chromatic()
@@ -25,11 +22,12 @@ class Scale:
         scale = []
         for interval in intervals:
             scale.append(chromatic[i])
-            if interval == 'm':
-                i += 1
-            elif interval == 'M':
-                i += 2
-            elif interval == 'A':
-                i += 3
+            i += self.INTERVAL_DISTANCES.index(interval)+1
 
         return scale
+
+    def _get_scale_by_tonic(self, tonic):
+        if self.tonic in self.DIATONIC_SHARPS:
+            return self.CHROMATIC_SHARPS
+        else:
+            return self.CHROMATIC_FLATS
