dd0c0c868345995c286de2fcda9af083be519e70
Paul Haddad <paulh16@gmail.com>
2020-11-05 07:02:38 -0500

    Go - Isogram
    * Remove alternate version

--- exercism/go/isogram/isogram.go
+++ exercism/go/isogram/isogram.go
@@ -6,7 +6,6 @@ import (
 )
 
 // IsIsogram returns whether a word or phrase is an isogram
-// Map version
 func IsIsogram(str string) bool {
 	lettersSeen := make(map[rune]bool)
 
@@ -22,22 +21,3 @@ func IsIsogram(str string) bool {
 
 	return true
 }
-
-// IsIsogram returns whether a word or phrase is an isogram
-// Array version
-// func IsIsogram(str string) bool {
-// 	var letterPresent [26]int
-//
-// 	for _, char := range strings.ToLower(str) {
-// 		if unicode.IsLetter(char) {
-// 			offset := int(char) - 'a'
-// 			if letterPresent[offset] > 0 {
-// 				return false
-// 			}
-//
-// 			letterPresent[offset] = 1
-// 		}
-// 	}
-//
-// 	return true
-// }
