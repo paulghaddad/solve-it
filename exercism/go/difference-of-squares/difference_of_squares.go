d362a08d463590e91e492edcc62a4077fa913916
Paul Haddad <paulh16@gmail.com>
2020-11-06 07:21:33 -0500

    Go - Difference of Squares - 1

---
+++ exercism/go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,25 @@
+package diffsquares
+
+// SquareOfSum returns the square of the sum from 1 to n
+func SquareOfSum(n int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	return sum * sum
+}
+
+// SumOfSquares returns the sum of the squares from 1 to n
+func SumOfSquares(n int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i * i
+	}
+	return sum
+}
+
+// Difference returns the difference between the square of sums and the sum of
+// squares
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
