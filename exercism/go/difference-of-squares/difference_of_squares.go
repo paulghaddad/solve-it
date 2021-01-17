e7c22cae5a76bd1af0755517a8a3bfc5d5b52c6a
Paul Haddad <paulh16@gmail.com>
2021-01-17 07:17:53 -0500

    Go - Difference of Squares - 3
    * Use shortcut for sum of squares

--- exercism/go/difference-of-squares/difference_of_squares.go
+++ exercism/go/difference-of-squares/difference_of_squares.go
@@ -8,11 +8,7 @@ func SquareOfSum(n int) int {
 
 // SumOfSquares returns the sum of the squares from 1 to n
 func SumOfSquares(n int) int {
-	sum := 0
-	for i := 1; i <= n; i++ {
-		sum += i * i
-	}
-	return sum
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 // Difference returns the difference between the square of sums and the sum of
