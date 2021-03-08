dd2c85c08b1317e619f93240e987215ca5479987
Paul Haddad <paulh16@gmail.com>
2021-03-08 14:42:02 -0500

    Exercism - C - Darts
    * V2
    * Add enums

--- exercism/c/darts/src/darts.c
+++ exercism/c/darts/src/darts.c
@@ -1,15 +1,17 @@
 #include "darts.h"
 #include <math.h>
 
+enum scores { INNER_CIRCLE_SCORE=10, MIDDLE_CIRCLE_SCORE=5, OUTER_CIRCLE_SCORE=1, NO_SCORE=0};
+
 int score(coordinate_t position) {
   float radius = sqrt(powf(position.x, 2) + powf(position.y, 2));
 
   if (radius <= 10.0 && radius > 5.0)
-    return 1;
+    return OUTER_CIRCLE_SCORE;
   else if (radius <= 5.0 && radius > 1.0)
-    return 5;
+    return MIDDLE_CIRCLE_SCORE;
   else if (radius <= 1.0 && radius >= 0.0)
-    return 10;
+    return INNER_CIRCLE_SCORE;
   else
-    return 0;
+    return NO_SCORE;
 }
