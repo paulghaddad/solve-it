7d4688afc9251632bf13e5291601a41e39a2f75b
Paul Haddad <paulh16@gmail.com>
2021-03-08 15:10:44 -0500

    Exercism - C - Resistor Color
    * V2
    * Use static instead of malloc

--- exercism/c/resistor-color/src/resistor_color.c
+++ exercism/c/resistor-color/src/resistor_color.c
@@ -1,20 +1,14 @@
 #include "resistor_color.h"
-#include <stdlib.h>
 
 int color_code(resistor_band_t code) {
   return code;
 }
 
 resistor_band_t* colors() {
-   resistor_band_t default_colors[] = {
+   static resistor_band_t colors[] = {
       BLACK, BROWN, RED, ORANGE, YELLOW,
       GREEN, BLUE, VIOLET, GREY, WHITE
    };
-
-   resistor_band_t* colors = malloc(sizeof(resistor_band_t) * 10);
-
-   for (int i = 0; i < 10; i++)
-     colors[i] = default_colors[i];
 
    return colors;
 }
