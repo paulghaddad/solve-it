5e91c6a280a4705f32285e3fe8801f2df926a21c
Paul Haddad <paulh16@gmail.com>
2021-04-08 08:28:59 -0400

    Exercism - Luhn
    * Version 2

--- exercism/go/luhn/luhn.go
+++ exercism/go/luhn/luhn.go
@@ -2,51 +2,41 @@ package luhn
 
 import (
 	"strings"
-	"unicode"
 	"unicode/utf8"
 )
 
 // Valid returns whether a given number is a valid Luhn checksum
 func Valid(input string) bool {
-	input = strings.TrimSpace(input)
-
-	if utf8.RuneCountInString(input) <= 1 {
+	if utf8.RuneCountInString(strings.TrimSpace(input)) <= 1 {
 		return false
 	}
 
-	// convert to slice of ints
-	digits := []int{}
-	for _, char := range input {
-		if validLuhnChar(char) {
+	sum := 0
+	for i, j := len(input)-1, 0; i >= 0; i-- {
+		char := input[i]
+
+		if char == ' ' {
+			continue
+		}
+
+		if char < '0' || char > '9' {
 			return false
 		}
 
-		if unicode.IsDigit(char) {
-			digits = append(digits, int(char)-'0')
-		}
-	}
+		digit := int(char - '0')
 
-	// double every second number from right
-	// sum all the numbers
-	sum := 0
-	j := 0
-	for i := len(digits) - 1; i >= 0; i-- {
 		if j%2 == 1 {
-			product := 2 * digits[i]
-			if product > 9 {
-				product -= 9
+			digit <<= 1
+			if digit > 9 {
+				digit -= 9
 			}
-
-			digits[i] = product
+			sum += digit
+		} else {
+			sum += digit
 		}
 
-		sum += digits[i]
 		j++
 	}
 
 	return sum%10 == 0
-}
-
-func validLuhnChar(char rune) bool {
-	return !unicode.IsDigit(char) && !unicode.IsSpace(char)
 }
