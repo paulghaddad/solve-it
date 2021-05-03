82caceb52f592b9fe4fb30924767dfbc05b2e3f4
Paul Haddad <paulh16@gmail.com>
2021-05-03 10:12:51 -0400

    Exercism - Go - Largest Series Product
    * V3: Continued refactoring to a simpler approach

--- exercism/go/largest-series-product/largest_series_product.go
+++ exercism/go/largest-series-product/largest_series_product.go
@@ -8,92 +8,46 @@ import "fmt"
 */
 
 func LargestSeriesProduct(digits string, span int) (int, error) {
-	if len(digits) < span {
+	switch {
+	case len(digits) < span:
 		return -1, fmt.Errorf("span must be smaller than string length")
-	}
-
-	if len(digits) == 0 {
+	case len(digits) == 0:
 		return 1, nil
-	}
-
-	if span < 0 {
+	case span == 0:
+		return 1, nil
+	case span < 0:
 		return -1, fmt.Errorf("span must be greater than zero")
 	}
 
-	if span == 0 {
-		return 1, nil
-	}
+	var product, largestProduct, window int
 
-	var i, product, largestProduct int
-	needProduct := true
-
-MAIN:
-	for i < len(digits) {
-		digit, err := getDigit(digits, i)
-		if err != nil {
-			return -1, err
+	for i := range digits {
+		digit := int(digits[i] - '0')
+		if digit < 0 || digit > 9 {
+			return -1, fmt.Errorf("digits input must only contain digits")
 		}
 
-		// skip over 0s and reset product
 		if digit == 0 {
-			needProduct = true
-			product = 0
-			i++
+			product, window = 0, 0
 			continue
 		}
 
-		if needProduct == true {
-			// find the product of three non-zero digits
-			for j := 0; j < span && i-j+span-1 < len(digits); j++ {
-				digit, err := getDigit(digits, i)
-				if err != nil {
-					return -1, err
-				}
-
-				if digit == 0 {
-					product = 0
-					i++
-					goto MAIN
-				}
-
-				if product == 0 {
-					product = 1
-				}
-
-				product *= digit
-				i++
+		if window < span {
+			if product == 0 {
+				product = 1
 			}
-
-			if product > largestProduct {
-				largestProduct = product
-			}
-			needProduct = false
-			continue
+			product *= digit
+			window++
+		} else {
+			digitToRemove := int(digits[i-span] - '0')
+			product /= digitToRemove
+			product *= digit
 		}
 
-		digitToRemove, err := getDigit(digits, i-span)
-		if err != nil {
-			return -1, err
-		}
-		// calculate current product of span
-		product /= digitToRemove
-		product *= digit
-
-		if product > largestProduct {
+		if window == span && product > largestProduct {
 			largestProduct = product
 		}
-
-		i++
 	}
 
 	return largestProduct, nil
-}
-
-func getDigit(digits string, i int) (int, error) {
-	digit := int(digits[i] - '0')
-	if digit < 0 || digit > 9 {
-		return -1, fmt.Errorf("digits input must only contain digits")
-	}
-
-	return digit, nil
 }
