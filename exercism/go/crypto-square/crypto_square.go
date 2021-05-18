d4baf4511b6486a71022541273605c1b7f841102
Paul Haddad <paulh16@gmail.com>
2021-05-18 07:54:37 -0400

    Exercism - Go - Crypto Square
    * V2 - use strings.Builder

--- exercism/go/crypto-square/crypto_square.go
+++ exercism/go/crypto-square/crypto_square.go
@@ -1,50 +1,53 @@
 package cryptosquare
 
 import (
+	"strings"
 	"unicode"
 )
 
 // Encode returns the crypto square encoding of the input text
 func Encode(s string) string {
-	var normS, encrypted []byte
+	var b, encrypted strings.Builder
 
 	for _, ch := range s {
 		lower := unicode.ToLower(ch)
 
 		if lower >= 'a' && lower <= 'z' || lower >= '0' && lower <= '9' {
-			normS = append(normS, byte(lower))
+			b.WriteByte(byte(lower))
 		}
 	}
 
 	// Determine c and r
 	var c, r int
-	for c = 1; c <= len(normS); c++ {
-		if c*(c-1) >= len(normS) {
+
+	for c = 1; c <= b.Len(); c++ {
+		if c*(c-1) >= b.Len() {
 			r = c - 1
 			break
 		}
 
-		if c*c >= len(normS) {
+		if c*c >= b.Len() {
 			r = c
 			break
 		}
 	}
 
 	// Add padding if necessary
-	spacesNeeded := c*r - len(normS)
+	spacesNeeded := c*r - b.Len()
 	for i := 0; i < spacesNeeded; i++ {
-		normS = append(normS, ' ')
+		b.WriteByte(' ')
 	}
 
+	normS := b.String()
 	for i := 0; i < c; i++ {
 		for j := 0; j < r; j++ {
-			encrypted = append(encrypted, normS[c*j+i])
+			encrypted.WriteByte(normS[c*j+i])
 		}
 
 		if i < c-1 {
-			encrypted = append(encrypted, ' ')
+			encrypted.WriteByte(' ')
 		}
 	}
 
-	return string(encrypted)
+	return encrypted.String()
 }
