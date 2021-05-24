aa4e892235e2486eaadbd5015454c9bb0e3bf902
Paul Haddad <paulh16@gmail.com>
2021-05-24 06:41:39 -0400

    Exercism - Go - Tree Building
    * V3

--- exercism/go/tree-building/tree_building.go
+++ exercism/go/tree-building/tree_building.go
@@ -18,46 +18,32 @@ type Node struct {
 
 // Build constructs the recursive data type for the tree
 func Build(records []Record) (*Node, error) {
-	if len(records) == 0 {
-		return nil, nil
-	}
-
-	// sort records by ID
 	sort.Slice(records, func(i, j int) bool {
 		return records[i].ID < records[j].ID
 	})
 
-	// Create tree root
-	rootRecord := records[0]
-	rootNode := Node{ID: rootRecord.ID}
-
-	// Create map of Node pointers
 	nodes := make(map[int]*Node)
-	nodes[rootRecord.ID] = &rootNode
 
-	// make sure there is a root node
-	if _, found := nodes[rootRecord.Parent]; !found {
-		return nil, fmt.Errorf("no root node")
-	}
-
-	// check for integrity of tree and cycles and build tree
-	for i := 1; i < len(records); i++ {
-		record := records[i]
-		step := record.ID - records[i-1].ID
-
-		if step != 1 {
-			return nil, fmt.Errorf("duplicate nodes")
+	for i, record := range records {
+		if i != record.ID {
+			return nil, fmt.Errorf("invalid tree")
 		}
 
-		if _, found := nodes[record.Parent]; found == false {
-			return nil, fmt.Errorf("cycle present")
+		if record.ID == 0 && record.Parent > 0 {
+			return nil, fmt.Errorf("root node has parent")
 		}
 
 		node := Node{ID: record.ID}
-		parent := nodes[record.Parent]
-		parent.Children = append(parent.Children, &node)
+		if record.Parent > 0 && record.Parent >= node.ID {
+			return nil, fmt.Errorf("cycle present")
+		}
+
+		if parent, found := nodes[record.Parent]; found {
+			parent.Children = append(parent.Children, &node)
+		}
+
 		nodes[record.ID] = &node
 	}
 
-	return &rootNode, nil
+	return nodes[0], nil
 }
