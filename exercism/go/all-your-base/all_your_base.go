113810e66fed433e03f6ac0270a5e69d38cee1cf
Paul Haddad <paulh16@gmail.com>
2021-06-16 08:48:41 -0400

    Exercism - Go - All your base
    * V2

--- exercism/go/all-your-base/all_your_base.go
+++ exercism/go/all-your-base/all_your_base.go
@@ -2,13 +2,12 @@ package allyourbase
 
 import (
 	"fmt"
-	"math"
 )
 
 // ConvertToBase converts a number from one base to another
 func ConvertToBase(inputBase int, inputDigits []int, outputBase int) ([]int, error) {
 	var outputDigits []int
-	var n, r int
+	var sum, r int
 
 	if inputBase < 2 {
 		return outputDigits, fmt.Errorf("input base must be >= 2")
@@ -23,27 +22,21 @@ func ConvertToBase(inputBase int, inputDigits []int, outputBase int) ([]int, err
 	}
 
 	// convert to decimal
-	for i, j := len(inputDigits)-1, 0; i >= 0; i, j = i-1, j+1 {
-		inputDigit := inputDigits[i]
-		if inputDigit < 0 || inputDigit >= inputBase {
+	for _, d := range inputDigits {
+		if d < 0 || d >= inputBase {
 			return []int{}, fmt.Errorf("all digits must satisfy 0 <= d < input base")
 		}
-		n += inputDigit * int(math.Pow(float64(inputBase), float64(j)))
+		sum = sum*inputBase + d
 	}
 
-	if n == 0 {
+	if sum == 0 {
 		return []int{0}, nil
 	}
 
 	// convert from decimal to output base
-	for n > 0 {
-		n, r = n/outputBase, n%outputBase
-		outputDigits = append(outputDigits, r)
-	}
-
-	// reverse digits
-	for i, j := 0, len(outputDigits)-1; i < j; i, j = i+1, j-1 {
-		outputDigits[i], outputDigits[j] = outputDigits[j], outputDigits[i]
+	for sum > 0 {
+		sum, r = sum/outputBase, sum%outputBase
+		outputDigits = append([]int{r}, outputDigits...)
 	}
 
 	return outputDigits, nil
